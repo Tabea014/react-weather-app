f776072a596c42b243610d10eed6f1029ae9fef1
Tabea014 <76948160+Tabea014@users.noreply.github.com>
2021-02-05 08:56:38 +0100

    Update Footer, open Links in a new tab

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
   return (
     <div className="git-link">
       <span>
-        Created by <a href="https://www.linkedin.com/in/tabea-schuchardt/"> Tabea Schuchardt </a> - view on
-        <a href="https://github.com/Tabea014/react-weather-app/tree/master/src"> Github </a>
+        Created by <a href="https://www.linkedin.com/in/tabea-schuchardt/" target="_blank"> Tabea Schuchardt </a> - view on
+        <a href="https://github.com/Tabea014/react-weather-app/tree/master/src" target="_blank" > Github </a>
       </span>
     </div>
   );
